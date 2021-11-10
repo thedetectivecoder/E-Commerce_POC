251094bdda7123bc402aee5b08718b12cd7de453
Yatin Burhmi <yatinburhmi@Yatins-MacBook-Air.local>
2021-11-10 15:32:04 -0500

    Added fiter functionality

--- products.js
+++ products.js
@@ -2,42 +2,47 @@
   var products = [
     {
         productName: "CL1",
-        productPrice: "$19.99",
-        productImg: "https://images.unsplash.com/photo-1489987707025-afc232f7ea0f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Y2xvdGh8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+        productPrice: "$21.99",
+        productImg: "https://images.unsplash.com/photo-1489987707025-afc232f7ea0f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Y2xvdGh8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+        category: "Men"
     },
     {
-        productName: "CL1",
-        productPrice: "$19.99",
-        productImg: "https://images.unsplash.com/photo-1489987707025-afc232f7ea0f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Y2xvdGh8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+        productName: "CL2",
+        productPrice: "$29.99",
+        productImg: "https://images.unsplash.com/photo-1489987707025-afc232f7ea0f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Y2xvdGh8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+        category: "Women"
     },{
-        productName: "CL1",
-        productPrice: "$19.99",
-        productImg: "https://images.unsplash.com/photo-1489987707025-afc232f7ea0f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Y2xvdGh8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+        productName: "CL3",
+        productPrice: "$49.99",
+        productImg: "https://images.unsplash.com/photo-1489987707025-afc232f7ea0f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Y2xvdGh8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+        category: "Women"
     },{
-        productName: "CL1",
+        productName: "CL4",
         productPrice: "$19.99",
-        productImg: "https://images.unsplash.com/photo-1489987707025-afc232f7ea0f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Y2xvdGh8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+        productImg: "https://images.unsplash.com/photo-1489987707025-afc232f7ea0f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Y2xvdGh8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+        category: "Men"
     },{
-        productName: "CL1",
-        productPrice: "$19.99",
-        productImg: "https://images.unsplash.com/photo-1489987707025-afc232f7ea0f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Y2xvdGh8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+        productName: "CL5",
+        productPrice: "$10.99",
+        productImg: "https://images.unsplash.com/photo-1489987707025-afc232f7ea0f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Y2xvdGh8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+        category: "Kids"
     },
 ]
 
-function renderProducts(){  
-    console.log(products)
+function renderProducts(products){ 
+    console.log("Products:: ", products) 
     products.forEach(product => {
-        console.log("P : ", product)
         renderEachProduct(product)});
 }
 
 
 //here ${product.attribVal} is template literal.
+const findDiv = document.getElementById('clothes-box');
 
-console.log("FD out: ", findDiv)
 function renderEachProduct(product){
+    console.log("redP", product);
     const findDiv = document.getElementById('clothes-box');
-    console.log("ProEach: ", product)
+    console.log(findDiv)
     const newElement = document.createElement("div")
     newElement.className = "content"
     newElement.innerHTML = `
@@ -46,10 +51,25 @@ function renderEachProduct(product){
                 <img src="${product.productImg}" class="image">  
                 <h2>${product.productName}</h2>
                 <p>Price: $${product.productPrice}</p>
+                <p>Category: $${product.category}</p>
                 <button class="add-item>Add to cart</button>
             </div>
         </div>
     `
     findDiv.append(newElement)
+}
+
+function filterProducts(category){
+    console.log("Cat", category)
+    document.getElementById('clothes-box').innerHTML = "";
+    if(category == "All"){
+        renderProducts()
+    }else{
+        const filteredProducts = products.filter(product => product.category == category);
+        console.log("FP : ",filteredProducts);
+        document.getElementById('clothes-box').innerHTML = "";
+        renderProducts(filteredProducts);
+    }
+    
 }
 
